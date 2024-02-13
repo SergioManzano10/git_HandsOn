7d596413e590628aa64239eb18809585097d6239
Sergio <smazano250800@gmail.com>
2024-02-13 10:28:35 +0100

    Nucleotide percentage added

--- seqClass.py
+++ seqClass.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-
 import sys, re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser = ArgumentParser(description='Compute the percentage of each nucleotide in a DNA or RNA sequence')
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
@@ -13,16 +11,31 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
+args.seq = args.seq.upper()
+
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq) and not re.search('U', args.seq): # Ensures to have a DNA sequence
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq) and not re.search('T', args.seq): # Ensures to have a RNA sequence
-        print ('The sequence is RNA')
+    if re.search('T', args.seq) and not re.search('U', args.seq): 
+        print('The sequence is DNA')
+    elif re.search('U', args.seq) and not re.search('T', args.seq): 
+        print('The sequence is RNA')
     else:
-        print ('The sequence can be DNA or RNA')
+        print('The sequence can be DNA or RNA')
+    
+    # Calcular el porcentaje de cada nucleótido
+    total_length = len(args.seq)
+    nucleotide_counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'U': 0}
+    
+    # Contar la cantidad de cada nucleótido en la secuencia
+    for nucleotide in args.seq:
+        nucleotide_counts[nucleotide] += 1
+    
+    # Calcular el porcentaje de cada nucleótido
+    print("Percentage of each nucleotide:")
+    for nucleotide, count in nucleotide_counts.items():
+        percentage = (count / total_length) * 100
+        print(f"{nucleotide}: {percentage:.2f}%")
 else:
-    print ('The sequence is not DNA nor RNA')
+    print('The sequence is not DNA nor RNA')
 
 if args.motif:
     args.motif = args.motif.upper()
